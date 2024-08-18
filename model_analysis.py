cdca9028f3da5bc7ef396cd467f43f1635a9ed7c
pragmaAmagi <166608346+pragmaAmagi@users.noreply.github.com>
2024-08-18 02:50:01 -0600

    Update model_analysis.py
    copyright

--- model_analysis.py
+++ model_analysis.py
@@ -1,3 +1,13 @@
+#Copyright (c) [2024] ACME Media Limited
+#All rights reserved.
+#Permission to use, copy, modify, and distribute this software and its documentation for any purpose and without fee is hereby granted,
+#provided that the above copyright notice appear in all copies and that both the copyright notice and this permission notice appear in supporting documentation, 
+#and that the name of ACMe Media Limited not be used in advertising or publicity pertaining to distribution of the software without specific, written prior permission.
+
+#ACME Media Limited DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, 
+#IN NO EVENT SHALL ACME Media Limited BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM 
+#LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
 import numpy as np
 import matplotlib.pyplot as plt
 from mathematical_model import pi_i, cost_function, total_profit, get_mixed_strategy
